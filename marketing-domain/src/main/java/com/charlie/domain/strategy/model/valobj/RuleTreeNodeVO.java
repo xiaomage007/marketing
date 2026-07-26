c04e0277dd3dbbe4a156b716354f2d5892cbc580
xiaomage007 <m_a_hui@126.com>
2026-07-26 16:18:25 +0800

    抽奖规则树模型结构设计

---
+++ marketing-domain/src/main/java/com/charlie/domain/strategy/model/valobj/RuleTreeNodeVO.java
@@ -0,0 +1,44 @@
+package com.charlie.domain.strategy.model.valobj;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @ClassName: RuleTreeNodeVO
+ * @Description: 规则树节点对象
+ * @Author: Charlie
+ * @Date: 2026/7/26 15:18
+ * @Version: 1.0
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RuleTreeNodeVO {
+    /**
+     * 规则树ID
+     */
+    private Integer treeId;
+    /**
+     * 规则Key
+     */
+    private String ruleKey;
+    /**
+     * 规则描述
+     */
+    private String ruleDesc;
+    /**
+     * 规则比值
+     */
+    private String ruleValue;
+
+    /**
+     * 规则连线
+     */
+    private List<RuleTreeNodeLineVO> treeNodeLineVOList;
+
+}
---
+++ marketing-domain/src/main/java/com/charlie/domain/strategy/model/valobj/RuleTreeVO.java
@@ -0,0 +1,46 @@
+package com.charlie.domain.strategy.model.valobj;
+
+import com.sun.tools.javac.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * @ClassName: RuleTreeVO
+ * @Description: 规则树对象【注意；不具有唯一ID，不需要改变数据库结果的对象，可以被定义为值对象】
+ * @Author: Charlie
+ * @Date: 2026/7/26 15:24
+ * @Version: 1.0
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RuleTreeVO {
+
+    /**
+     * 规则树ID
+     */
+    private Integer treeId;
+    /**
+     * 规则树名称
+     */
+    private String treeName;
+    /**
+     * 规则树描述
+     */
+    private String treeDesc;
+    /**
+     * 规则根节点
+     */
+    private String treeRootRuleNode;
+
+    /**
+     * 规则节点
+     */
+    private Map<String, RuleTreeNodeVO> treeNodeMap;
+
+}
