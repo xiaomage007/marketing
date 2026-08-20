8efce0dc9683be3870ac458d71faa80e52092edb
xiaomage007 <m_a_hui@126.com>
2026-08-20 08:11:11 +0800

    第16节：引入MQ处理活动SKU库存一致性

---
+++ marketing-domain/src/main/java/com/charlie/domain/activity/service/armory/IActivityDispatch.java
@@ -0,0 +1,21 @@
+package com.charlie.domain.activity.service.armory;
+
+import java.util.Date;
+
+/**
+ * @description: 活动调度【扣减库存】
+ * @author: Charlie
+ * @date: 2026/8/20 7:46
+ */
+public interface IActivityDispatch {
+
+    /**
+     * 根据策略ID和奖品ID，扣减奖品缓存库存
+     *
+     * @param sku 互动SKU
+     * @param endDateTime 活动结束时间，根据结束时间设置加锁的key为结束时间
+     * @return 扣减结果
+     */
+    boolean subtractionActivitySkuStock(Long sku, Date endDateTime);
+
+}
