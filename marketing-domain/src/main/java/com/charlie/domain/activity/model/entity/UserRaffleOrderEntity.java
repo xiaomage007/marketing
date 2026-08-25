aab2f5dad6f72854be0a5193fee01260aaab7ab7
xiaomage007 <m_a_hui@126.com>
2026-08-25 08:09:58 +0800

    第18节：领取活动扣减账户额度

--- marketing-domain/src/main/java/com/charlie/domain/activity/model/entity/UserRaffleOrderEntity.java
+++ marketing-domain/src/main/java/com/charlie/domain/activity/model/entity/UserRaffleOrderEntity.java
@@ -1,9 +1,12 @@
 package com.charlie.domain.activity.model.entity;
 
+import com.charlie.domain.activity.model.valobj.UserRaffleOrderStateVO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 /**
  * @description: 用户抽奖订单实体对象
@@ -16,6 +19,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserRaffleOrderEntity {
 
-
+    /** 用户ID */
+    private String userId;
+    /** 活动ID */
+    private Long activityId;
+    /** 活动名称 */
+    private String activityName;
+    /** 抽奖策略ID */
+    private Long strategyId;
+    /** 订单ID */
+    private String orderId;
+    /** 下单时间 */
+    private Date orderTime;
+    /** 订单状态；create-创建、used-已使用、cancel-已作废 */
+    private UserRaffleOrderStateVO orderState;
 
 }
--- marketing-domain/src/main/java/com/charlie/domain/activity/service/IRaffleActivityPartakeService.java
+++ marketing-domain/src/main/java/com/charlie/domain/activity/service/IRaffleActivityPartakeService.java
@@ -1,5 +1,8 @@
 package com.charlie.domain.activity.service;
 
+import com.charlie.domain.activity.model.entity.PartakeRaffleActivityEntity;
+import com.charlie.domain.activity.model.entity.UserRaffleOrderEntity;
+
 /**
  * @description: 抽奖活动参与服务
  * @author: Charlie
@@ -7,6 +10,12 @@ package com.charlie.domain.activity.service;
  */
 public interface IRaffleActivityPartakeService {
 
-
+    /**
+     * 创建抽奖单；用户参与抽奖活动，扣减活动账户库存，产生抽奖单。如存在未被使用的抽奖单则直接返回已存在的抽奖单。
+     *
+     * @param partakeRaffleActivityEntity 参与抽奖活动实体对象
+     * @return 用户抽奖订单实体对象
+     */
+    UserRaffleOrderEntity createOrder(PartakeRaffleActivityEntity partakeRaffleActivityEntity);
 
 }
