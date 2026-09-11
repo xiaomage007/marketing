d8d3228a66f192565e331463a69cc267ba5cc14c
xiaomage007 <m_a_hui@126.com>
2026-09-11 08:45:36 +0800

    第24节：规则完善和应用接口实现 判断是否签到接口

--- marketing-domain/src/main/java/com/charlie/domain/rebate/service/IBehaviorRebateService.java
+++ marketing-domain/src/main/java/com/charlie/domain/rebate/service/IBehaviorRebateService.java
@@ -1,6 +1,7 @@
 package com.charlie.domain.rebate.service;
 
 import com.charlie.domain.rebate.model.entity.BehaviorEntity;
+import com.charlie.domain.rebate.model.entity.BehaviorRebateOrderEntity;
 
 import java.util.List;
 
@@ -19,4 +20,5 @@ public interface IBehaviorRebateService {
      */
     List<String> createOrder(BehaviorEntity behaviorEntity);
 
+    List<BehaviorRebateOrderEntity> queryOrderByOutBusinessNo(String userId, String outBusinessNo);
 }
