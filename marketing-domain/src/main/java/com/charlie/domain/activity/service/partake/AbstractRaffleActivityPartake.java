217b843b71a7239cc12d24da07975ac1c22e8579
xiaomage007 <m_a_hui@126.com>
2026-09-17 07:54:12 +0800

    fix：增加 redis 锁，和账户修改前的查询判断，避免产生间隙锁问题

--- marketing-domain/src/main/java/com/charlie/domain/activity/service/partake/AbstractRaffleActivityPartake.java
+++ marketing-domain/src/main/java/com/charlie/domain/activity/service/partake/AbstractRaffleActivityPartake.java
@@ -41,11 +41,12 @@ public abstract class AbstractRaffleActivityPartake implements IRaffleActivityPa
         String userId = partakeRaffleActivityEntity.getUserId();
         Long activityId = partakeRaffleActivityEntity.getActivityId();
         Date currentDate = new Date();
-
+        log.info("创建活动抽奖单开始 userId:{} activityId:{}", userId, activityId);
         // 1. 活动查询
         ActivityEntity activityEntity = activityRepository.queryRaffleActivityByActivityId(activityId);
         // 校验；活动状态
         if (!ActivityStateVO.open.equals(activityEntity.getState())) {
+            log.error("创建活动抽奖单失败，活动状态未开启 activityId:{} state:{}", activityId, activityEntity.getState());
             throw new AppException(ResponseCode.ACTIVITY_STATE_ERROR.getCode(), ResponseCode.ACTIVITY_STATE_ERROR.getInfo());
         }
         // 校验；活动日期「开始时间 <- 当前时间 -> 结束时间」
@@ -55,7 +56,7 @@ public abstract class AbstractRaffleActivityPartake implements IRaffleActivityPa
         // 2. 查询未被使用的活动参与订单记录
         UserRaffleOrderEntity userRaffleOrderEntity = activityRepository.queryNoUsedRaffleOrder(partakeRaffleActivityEntity);
         if (null != userRaffleOrderEntity) {
-            log.info("创建参与活动订单 userId:{} activityId:{} userRaffleOrderEntity:{}", userId, activityId, JSON.toJSONString(userRaffleOrderEntity));
+            log.info("创建参与活动订单存在 userId:{} activityId:{} userRaffleOrderEntity:{}", userId, activityId, JSON.toJSONString(userRaffleOrderEntity));
             return userRaffleOrderEntity;
         }
         // 3. 额度账户过滤&返回账户构建对象
@@ -66,6 +67,7 @@ public abstract class AbstractRaffleActivityPartake implements IRaffleActivityPa
         createPartakeOrderAggregate.setUserRaffleOrderEntity(userRaffleOrder);
         // 6. 保存聚合对象 - 一个领域内的一个聚合是一个事务操作
         activityRepository.saveCreatePartakeOrderAggregate(createPartakeOrderAggregate);
+        log.info("创建活动抽奖单完成 userId:{} activityId:{} orderId:{}", userId, activityId, userRaffleOrder.getOrderId());
         return userRaffleOrder;
     }
 
