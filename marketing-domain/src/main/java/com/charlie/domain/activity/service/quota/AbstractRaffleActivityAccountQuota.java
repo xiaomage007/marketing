7a783f62f7c83535e736eb46d50d9e744f0494d0
xiaomage007 <m_a_hui@126.com>
2026-09-21 08:25:07 +0800

    营销服务 第28节：积分应用场景接口实现

--- marketing-domain/src/main/java/com/charlie/domain/activity/service/quota/AbstractRaffleActivityAccountQuota.java
+++ marketing-domain/src/main/java/com/charlie/domain/activity/service/quota/AbstractRaffleActivityAccountQuota.java
@@ -41,27 +41,33 @@ public abstract class AbstractRaffleActivityAccountQuota extends RaffleActivityA
         }
 
         // 2. 查询未支付订单「一个月以内的未支付订单」
-        UnpaidActivityOrderEntity unpaidCreditOrder =  activityRepository.queryUnpaidActivityOrder(skuRechargeEntity);
+        UnpaidActivityOrderEntity unpaidCreditOrder = activityRepository.queryUnpaidActivityOrder(skuRechargeEntity);
         if (null != unpaidCreditOrder) return unpaidCreditOrder;
 
-        // 2. 查询基础信息
-        // 2.1 通过sku查询活动信息
+        // 3. 查询基础信息「sku、活动、次数」
         ActivitySkuEntity activitySkuEntity = queryActivitySku(sku);
-        // 2.2 查询活动信息
         ActivityEntity activityEntity = queryRaffleActivityByActivityId(activitySkuEntity.getActivityId());
-        // 2.3 查询次数信息（用户在活动上可参与的次数）
         ActivityCountEntity activityCountEntity = queryRaffleActivityCountByActivityCountId(activitySkuEntity.getActivityCountId());
-        // 3. 活动动作规则校验 「过滤失败则直接抛异常」- 责任链扣减sku库存
+
+        // 4. 活动动作规则校验 「过滤失败则直接抛异常」- 责任链扣减sku库存
         IActionChain actionChain = actionChainFactory.openActionChain();
         actionChain.action(activitySkuEntity, activityEntity, activityCountEntity);
-        // 4. 构建订单聚合对象
+
+        // 5. 构建订单聚合对象
         CreateQuotaOrderAggregate createOrderAggregate = buildOrderAggregate(skuRechargeEntity, activitySkuEntity, activityEntity, activityCountEntity);
-        // 5. 交易策略 - 【积分兑换，支付类订单】【返利无支付交易订单，直接充值到账】【订单状态变更交易类型策略】
+
+        // 6. 交易策略 - 【积分兑换，支付类订单】【返利无支付交易订单，直接充值到账】【订单状态变更交易类型策略】
         ITradePolicy tradePolicy = tradePolicyGroup.get(skuRechargeEntity.getOrderTradeType().getCode());
         tradePolicy.trade(createOrderAggregate);
 
-        // 6. 返回单号
-        return createOrderAggregate.getActivityOrderEntity().getOrderId();
+        // 7. 返回订单信息
+        ActivityOrderEntity activityOrderEntity = createOrderAggregate.getActivityOrderEntity();
+        return UnpaidActivityOrderEntity.builder()
+                .userId(userId)
+                .orderId(activityOrderEntity.getOrderId())
+                .outBusinessNo(activityOrderEntity.getOutBusinessNo())
+                .payAmount(activityOrderEntity.getPayAmount())
+                .build();
     }
 
     protected abstract CreateQuotaOrderAggregate buildOrderAggregate(SkuRechargeEntity skuRechargeEntity, ActivitySkuEntity activitySkuEntity, ActivityEntity activityEntity, ActivityCountEntity activityCountEntity);
