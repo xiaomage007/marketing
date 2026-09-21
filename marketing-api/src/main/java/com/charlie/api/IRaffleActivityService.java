d33f97170af9109bf21dbd03812b76888379b019
xiaomage007 <m_a_hui@126.com>
2026-09-21 09:26:15 +0800

    营销服务 第28节：积分应用场景接口实现

--- marketing-api/src/main/java/com/charlie/api/IRaffleActivityService.java
+++ marketing-api/src/main/java/com/charlie/api/IRaffleActivityService.java
@@ -1,10 +1,10 @@
 package com.charlie.api;
 
-import com.charlie.api.dto.ActivityDrawRequestDTO;
-import com.charlie.api.dto.ActivityDrawResponseDTO;
-import com.charlie.api.dto.UserActivityAccountRequestDTO;
-import com.charlie.api.dto.UserActivityAccountResponseDTO;
+import com.charlie.api.dto.*;
 import com.charlie.api.response.Response;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @description: 抽奖活动服务
@@ -50,6 +50,30 @@ public interface IRaffleActivityService {
      * @return 返回结果「总额度、月额度、日额度」
      */
     Response<UserActivityAccountResponseDTO> queryUserActivityAccount(UserActivityAccountRequestDTO request);
+
+    /**
+     * 查询sku商品集合
+     *
+     * @param activityId 活动ID
+     * @return 商品集合
+     */
+    Response<List<SkuProductResponseDTO>> querySkuProductListByActivityId(Long activityId);
+
+    /**
+     * 查询用户积分值
+     *
+     * @param userId 用户ID
+     * @return 可用积分
+     */
+    Response<BigDecimal> queryUserCreditAccount(String userId);
+
+    /**
+     * 积分支付兑换商品
+     *
+     * @param request 请求对象「用户ID、商品ID」
+     * @return 兑换结果
+     */
+    Response<Boolean> creditPayExchangeSku(SkuProductShopCartRequestDTO request);
 
 
 }
---
+++ marketing-api/src/main/java/com/charlie/api/dto/SkuProductResponseDTO.java
@@ -0,0 +1,63 @@
+package com.charlie.api.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @description: sku商品对象
+ * @author: Charlie
+ * @date: 2026/9/21 9:24
+ */
+@Data
+public class SkuProductResponseDTO {
+
+    /**
+     * 商品sku
+     */
+    private Long sku;
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+    /**
+     * 活动个人参与次数ID
+     */
+    private Long activityCountId;
+    /**
+     * 库存总量
+     */
+    private Integer stockCount;
+    /**
+     * 剩余库存
+     */
+    private Integer stockCountSurplus;
+    /**
+     * 商品金额【积分】
+     */
+    private BigDecimal productAmount;
+
+    /**
+     * 活动商品数量
+     */
+    private ActivityCount activityCount;
+
+    @Data
+    public static class ActivityCount {
+        /**
+         * 总次数
+         */
+        private Integer totalCount;
+
+        /**
+         * 日次数
+         */
+        private Integer dayCount;
+
+        /**
+         * 月次数
+         */
+        private Integer monthCount;
+    }
+
+}
