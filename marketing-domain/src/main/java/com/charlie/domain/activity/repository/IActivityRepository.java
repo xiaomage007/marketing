611a8d9510855683a0adb686d64750532ac9de13
xiaomage007 <m_a_hui@126.com>
2026-09-21 08:30:45 +0800

    营销服务 第28节：积分应用场景接口实现

--- marketing-domain/src/main/java/com/charlie/domain/activity/repository/IActivityRepository.java
+++ marketing-domain/src/main/java/com/charlie/domain/activity/repository/IActivityRepository.java
@@ -60,4 +60,6 @@ public interface IActivityRepository {
     void updateOrder(DeliveryOrderEntity deliveryOrderEntity);
 
     UnpaidActivityOrderEntity queryUnpaidActivityOrder(SkuRechargeEntity skuRechargeEntity);
+
+    List<SkuProductEntity> querySkuProductEntityListByActivityId(Long activityId);
 }
--- marketing-infrastructure/src/main/java/com/charlie/infrastructure/persistent/repository/ActivityRepository.java
+++ marketing-infrastructure/src/main/java/com/charlie/infrastructure/persistent/repository/ActivityRepository.java
@@ -879,4 +879,30 @@ public class ActivityRepository implements IActivityRepository {
                 .build();
     }
 
+    @Override
+    public List<SkuProductEntity> querySkuProductEntityListByActivityId(Long activityId) {
+        List<RaffleActivitySku> raffleActivitySkus = raffleActivitySkuDao.queryActivitySkuListByActivityId(activityId);
+        List<SkuProductEntity> skuProductEntities = new ArrayList<>(raffleActivitySkus.size());
+        for (RaffleActivitySku raffleActivitySku : raffleActivitySkus) {
+            RaffleActivityCount raffleActivityCount = raffleActivityCountDao.queryRaffleActivityCountByActivityCountId(raffleActivitySku.getActivityCountId());
+
+            SkuProductEntity.ActivityCount activityCount = new SkuProductEntity.ActivityCount();
+            activityCount.setTotalCount(raffleActivityCount.getTotalCount());
+            activityCount.setMonthCount(raffleActivityCount.getMonthCount());
+            activityCount.setDayCount(raffleActivityCount.getDayCount());
+
+            skuProductEntities.add(SkuProductEntity.builder()
+                    .sku(raffleActivitySku.getSku())
+                    .activityId(raffleActivitySku.getActivityId())
+                    .activityCountId(raffleActivitySku.getActivityCountId())
+                    .stockCount(raffleActivitySku.getStockCount())
+                    .stockCountSurplus(raffleActivitySku.getStockCountSurplus())
+                    .productAmount(raffleActivitySku.getProductAmount())
+                    .activityCount(activityCount)
+                    .build());
+
+        }
+        return skuProductEntities;
+    }
+
 }
